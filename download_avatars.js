433d27a7785c602cf763d9dc21b642a232db7163
Tymm Schmitke <ifellinaholeonce@gmail.com>
2017-12-04 22:08:17 +0000

    add inline comments. Make command line arguments for repoOwner and repoName mandatory

--- download_avatars.js
+++ download_avatars.js
@@ -4,32 +4,51 @@ var GITHUB_TOKEN = require('./secrets.js').GITHUB_TOKEN;
 var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
+//Make command line repoOwner and repoName mandatory
+var checkArguments = function (repoOwner, repoName) {
+  if (repoOwner === undefined || repoOwner === '' || repoOwner === ' ') {
+    console.log("Please input a Repo Owner");
+  }
 
+  if (repoName === undefined || repoName === '' || repoName === ' ') {
+    console.log("Please input a Repo Name");
+  }
+};
+
+//Run the check against the inputs
+checkArguments(repoOwner, repoName);
+
+//HTTP request
 var getRepoContributors = function(repoOwner, repoName, callback) {
+  //HTTP Options - repoOwner and repoName input into URL
   var options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${GITHUB_TOKEN}`
     }
-};
+  };
 
+  //Make the request call
   request(options, function(err, res, body) {
     body = JSON.parse(body);
     callback(err, body);
   });
 };
 
+//Initiate the request function and set the callback function to handle the response
 getRepoContributors(repoOwner, repoName, function(err, res) {
   if (err) {
     console.log("Errors:", err);
   }
   res.forEach(function(user) {
     console.log("Downloading:", user.login, user.avatar_url);
+    //Download avatars to directory /avatars/ and name each file the user login
     downloadImageByUrl(user.avatar_url, `./avatars/${user.login}`);
   });
 });
 
+//Take the avatar URL from the request and save to filepath
 var downloadImageByUrl = function(url, filePath) {
   request(url)
     .on('error', function(err) {
@@ -38,6 +57,6 @@ var downloadImageByUrl = function(url, filePath) {
     .on('response', function (res) {
       console.log(url, res.statusCode, res.statusMessage);
     })
-    .pipe(fs.createWriteStream(filePath));
+    .pipe(fs.createWriteStream(filePath)); //Save the actual file to disc
 };
 
